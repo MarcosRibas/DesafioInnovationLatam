2dc812edcfc2485895fa6007b99aa88434ff2bde
Marcos Ribas <contatomarcosribas@gmail.com>
2021-08-19 00:47:43 -0300

    Finish

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 
-let music_list = [false,false]
+let music_list = [false, false]
 let saved = 0
 let dialog_lock = document.getElementById('dialog_lock')
 let playTitle = document.getElementById('playTitle')
 let buttons = []
 
-
-
-function play(music){    
-
-    playTitle.innerHTML=music
+function play(music) {
+    playTitle.innerHTML = music
     dialog_lock.removeAttribute("hidden")
-    
-
 }
-function closeDialog(){
+
+function closeDialog() {
     dialog_lock.setAttribute("hidden", "hidden")
-
 }
+
 
 function save(position) {
-    let counter = document.getElementById('counter')
+    
     let saveButton = document.getElementById(`saveButton${position}`)
-    if(music_list[position]){
-        saveButton.innerText= `Save`
-        saved -= 1
-        music_list[position] = false
-    }else{
-        saved += 1
-        music_list[position] = true
-        saveButton.innerText= `Saved`
-    }
-    counter.innerHTML = saved    
+    saveButton.setAttribute("class", "invisivel");
+    let saveButtonn = document.getElementById(`saveButtonn${position}`)
+    saveButtonn.setAttribute("class", "button");
+    count(position);
 }
 
+function undo(position) {
+    let saveButton = document.getElementById(`saveButtonn${position}`)
+    saveButton.setAttribute("class", "invisivel");
+    let saveButtonn = document.getElementById(`saveButton${position}`)
+    saveButtonn.setAttribute("class", "button");
+    count(position);
+}
 
-
-
-
-
+function count(position) {
+    let counter = document.getElementById('counter')
+    if (music_list[position]) {
+        saved -= 1
+        music_list[position] = false
+    } else {
+        saved += 1
+        music_list[position] = true
+    }
+    counter.innerHTML = saved
+}
